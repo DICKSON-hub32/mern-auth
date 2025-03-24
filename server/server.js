177f62ccd8c69c6ce9ae8cb9bedf50a9f79ea7a6
Dickson Esamai <mukweesamai@gmail.com>
2025-03-24 16:32:42 +0300

    User controller working just fine

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import { connect } from "mongoose";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
