4093f313fea2ea38defc27a377ddf0835c3b2bbf
Dickson Esamai <mukweesamai@gmail.com>
2025-03-25 16:24:20 +0300

    Imported useData from Backend

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const AppContent = createContext();
 
@@ -7,12 +10,22 @@ export const AppContextProvider = (props) => {
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(false);
 
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/data");
+      data.success ? setUserData(data.userData) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.response?.data?.message || "An error occurred");
+    }
+  };
+
   const value = {
     backendUrl,
     isLoggedin,
     setIsLoggedin,
     userData,
-    setUserData
+    setUserData,
+    getUserData
   };
 
   return (
